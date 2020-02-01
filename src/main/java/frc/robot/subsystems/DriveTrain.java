36f1d0a0a5de30f0a7ca2e364f5ecae74e582705
austin-li <austinmingli@gmail.com>
2020-01-31 20:35:52 -0800

    set brake neutral mode and add squareinputs parameter

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -8,6 +8,7 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
@@ -21,29 +22,31 @@ public class DriveTrain extends SubsystemBase {
    */
   private WPI_TalonSRX left = new WPI_TalonSRX(2);
   private WPI_TalonSRX right = new WPI_TalonSRX(3);
-  //private WPI_TalonSRX leftFollow = new WPI_TalonSRX(4);
-  //private WPI_TalonSRX rightFollow = new WPI_TalonSRX(5);
+  private WPI_TalonSRX leftFollow = new WPI_TalonSRX(4);
+  private WPI_TalonSRX rightFollow = new WPI_TalonSRX(5);
   private DifferentialDrive drive;
   public DriveTrain() {
+	left.setNeutralMode(NeutralMode.Brake);
+	leftFollow.setNeutralMode(NeutralMode.Brake);
+	right.setNeutralMode(NeutralMode.Brake);
+	rightFollow.setNeutralMode(NeutralMode.Brake);
     drive = new DifferentialDrive(left, right);
   }
 
   
-  public void tankDrive(double leftpower, double rightpower){
-    //left.set(ControlMode.PercentOutput, leftpower);
-	//right.set(ControlMode.PercentOutput, rightpower);
-	drive.tankDrive(leftpower, rightpower);
-    //leftFollow.set(ControlMode.Follower, 2);
-    //rightFollow.set(ControlMode.Follower, 3);
+  public void tankDrive(double left, double right, boolean squareInputs){
+	drive.tankDrive(left, right, squareInputs);
+    leftFollow.set(ControlMode.Follower, 2);
+    rightFollow.set(ControlMode.Follower, 3);
   }
-  public void arcadeDrive(double move, double turn) {
-	  drive.arcadeDrive(move, turn);
-	  //leftFollow.set(ControlMode.Follower, 2);
-	  //rightFollow.set(ControlMode.Follower, 3);
+  public void arcadeDrive(double move, double turn, boolean squareInputs) {
+	  drive.arcadeDrive(move, turn, squareInputs);
+	  leftFollow.set(ControlMode.Follower, 2);
+	  rightFollow.set(ControlMode.Follower, 3);
   }
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    tankDrive(-Robot.m_robotContainer.getLeftJoy().getY(), -Robot.m_robotContainer.getRightJoy().getY());
+    tankDrive(-Robot.m_robotContainer.getLeftJoy().getY(), -Robot.m_robotContainer.getRightJoy().getY(), true);
   }
 }
